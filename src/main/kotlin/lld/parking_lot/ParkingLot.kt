21d43aeaf8c468e36893791447ac82f7b495be9f
b.erkanli <bariserkanli0@gmail.com>
2024-10-25 23:54:07 +0300

    Refactor ParkingLot to Singleton pattern.

--- src/main/kotlin/lld/parking_lot/ParkingLot.kt
+++ src/main/kotlin/lld/parking_lot/ParkingLot.kt
@@ -1,8 +1,27 @@
 package lld.parking_lot
 
-class ParkingLot(private val floors: List<Floor>, private val entrances: List<Entrance>){
+class ParkingLot private constructor (
+    private val floors: List<Floor>,
+    private val entrances: List<Entrance>
+){
     private val tickets = mutableMapOf<String, Ticket>()
 
+    companion object{
+        @Volatile
+        private var instance: ParkingLot? = null
+
+        fun initialize(floors: List<Floor>, entrances: List<Entrance>){
+            synchronized(this){
+                if (instance == null){
+                    instance = ParkingLot(floors, entrances)
+                }
+            }
+        }
+        fun getInstance(): ParkingLot {
+            return instance ?: throw IllegalStateException("ParkingLot not initialized. Call initialize() first.")
+        }
+    }
+    
     fun addVehicle(vehicle: Vehicle, entranceId: String): Ticket? {
         val entrance = entrances.find { it.entranceId == entranceId } ?: return null
         for (floor in floors) {
