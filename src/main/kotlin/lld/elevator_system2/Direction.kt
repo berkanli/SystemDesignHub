e5a95fb3dec3199bf39cac533e356da74ca6ab79
b.erkanli <bariserkanli0@gmail.com>
2024-10-28 12:39:56 +0300

    create elevator_system2

---
+++ src/main/kotlin/lld/elevator_system2/Direction.kt
@@ -0,0 +1,7 @@
+package lld.elevator_system2
+
+enum class Direction {
+    IDLE,
+    UP,
+    DOWN
+}
---
+++ src/main/kotlin/lld/elevator_system2/ElevatorStatus.kt
@@ -0,0 +1,6 @@
+package lld.elevator_system2
+
+enum class ElevatorStatus {
+    OCCUPIED,
+    EMPTY
+}
