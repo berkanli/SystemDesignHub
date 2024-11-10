417f2ff87bd0f5c51766f74e2e21d1468f740de0
b.erkanli <bariserkanli0@gmail.com>
2024-11-10 22:07:41 +0300

    create cache_aside_caching design

---
+++ src/main/kotlin/lld/caching/cache_aside/main.kt
@@ -0,0 +1,19 @@
+package lld.caching.cache_aside
+
+fun main() {
+    val cache = InMemoryCache<Int, String>()
+    val dataSource = ExampleDataSource()
+    val cacheAside = CacheAside(cache, dataSource)
+
+    // Populate the database
+    dataSource.updateDataSource(1, "Cached Data")
+
+    // Retrieve data (fetches from data source and caches it)
+    println(cacheAside.get(1))  // Output: Cached Data
+
+    // Update data and invalidate cache
+    cacheAside.update(1, "Updated Data")
+
+    // Retrieve data (fetches updated data from data source)
+    println(cacheAside.get(1))  // Output: Updated Data
+}
